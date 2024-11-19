9ec3f88070a7ddbc6e958e769c085016d71a2e3b
Pein <852128461@qq.com>
2024-11-19 10:24:04 +0000

    Remove /pretrained folder from tracking and update .gitignore

--- download_pretrained_weights.py
+++ download_pretrained_weights.py
@@ -8,11 +8,11 @@ os.makedirs('./pretrained', exist_ok=True)
 
 torch.hub.set_dir('./pretrained')
 
-dinov2_vitb14 = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitb14')
+dinov2_vitb14 = torch.hub.load('/data/training_code/Pein/dinov2/pretrained/facebookresearch_dinov2_main', 'dinov2_vitb14',trust_repo=True, source='local')
 dinov2_vitb14.eval()
 
 # Set device to CPU and move model to device
-device = torch.device('cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 dinov2_vitb14.to(device)
 
 test_img_path = '/data/training_code/Pein/dinov2/mixed_data_root/bbu_grounding_wire_cleaned/test/0000212/0000212-1.jpeg'
